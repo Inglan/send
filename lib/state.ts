8b40addd9e12d2161f0b0bcaf1f9a7445ff6ccce
Ingo Wolf <me@ingowolf.au>
2025-10-14 16:49:13 +1100

    add createsession action in state

--- lib/state.ts
+++ lib/state.ts
@@ -1,11 +1,21 @@
+import { api } from "@/convex/_generated/api";
+import { ReactMutation, useMutation } from "convex/react";
 import { create } from "zustand";
 
 interface AppState {
   sessionId: string;
   setSessionId: (id: string) => void;
+  createSession: (
+    text: string,
+    createSessionMutation: ReactMutation<typeof api.sessions.createWithCode>,
+  ) => Promise<void>;
 }
 
 export const useAppState = create<AppState>((set) => ({
   sessionId: "",
-  setSessionId: (id: string) => set({ sessionId: id }),
+  setSessionId: (id) => set({ sessionId: id }),
+  createSession: async (text, createSessionMutation) => {
+    const session = await createSessionMutation({ content: text });
+    set({ sessionId: session.sessionId });
+  },
 }));
