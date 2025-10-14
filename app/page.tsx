113470679868b7f5f5a1c4d2ff815a43c3a1c0d1
Ingo Wolf <me@ingowolf.au>
2025-10-14 16:53:48 +1100

    show code input when no sessionid

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import CodeInput from "@/components/code-input";
+import { useAppState } from "@/lib/state";
 
 export default function Home() {
+  const sessionId = useAppState((state) => state.sessionId);
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <CodeInput />
+      {!sessionId && <CodeInput />}
     </div>
   );
 }
