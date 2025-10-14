cd6510ac306edf30fdaf454386ae60798aeaa139
Ingo Wolf <me@ingowolf.au>
2025-10-14 13:44:18 +1100

    update schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,7 +2,11 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  sessions: defineTable({
+  sessionCodes: defineTable({
     code: v.string(),
+    session: v.id("sessions"),
+  }),
+  sessions: defineTable({
+    content: v.string(),
   }),
 });
