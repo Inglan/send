a5b6da66675145686a75f7066c0057fc8b8ec11d
Ingo Wolf <me@ingowolf.au>
2025-10-14 16:18:32 +1100

    control input and force upper case

--- components/code-input.tsx
+++ components/code-input.tsx
@@ -7,10 +7,18 @@ import {
   InputOTPSlot,
 } from "@/components/ui/input-otp";
 import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
+import { useState } from "react";
 
 export default function CodeInput() {
+  const [inputtedCode, setInputtedCode] = useState("");
+
   return (
-    <InputOTP maxLength={4} pattern={REGEXP_ONLY_DIGITS_AND_CHARS}>
+    <InputOTP
+      maxLength={4}
+      pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+      value={inputtedCode}
+      onChange={(value) => setInputtedCode(value.toUpperCase())}
+    >
       <InputOTPGroup>
         <InputOTPSlot index={0} />
         <InputOTPSlot index={1} />
