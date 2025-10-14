c751b9c5914edd73150eb70d03f81f5bb885731f
Ingo Wolf <me@ingowolf.au>
2025-10-14 16:17:09 +1100

    start working on code input

---
+++ components/code-input.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
+
+export default function CodeInput() {
+  return (
+    <InputOTP maxLength={4} pattern={REGEXP_ONLY_DIGITS_AND_CHARS}>
+      <InputOTPGroup>
+        <InputOTPSlot index={0} />
+        <InputOTPSlot index={1} />
+        <InputOTPSlot index={2} />
+        <InputOTPSlot index={3} />
+      </InputOTPGroup>
+    </InputOTP>
+  );
+}
