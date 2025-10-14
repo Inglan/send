39d540ee46216f1563c7a1f347efb10f15a3f458
Ingo Wolf <me@ingowolf.au>
2025-10-14 15:56:09 +1100

    finish function

--- convex/sessions.ts
+++ convex/sessions.ts
@@ -6,8 +6,25 @@ export const createWithCode = mutation({
     content: v.string(),
   },
   handler: async (ctx, args) => {
-    const sessionId = await ctx.db.insert("sessions", { content: args.content });
-    const sessionCode = await ctx.db.insert("sessionCodes", { session: sessionId, code: Math.random().toString(36).substring(2, 15) });
+    const sessionId = await ctx.db.insert("sessions", {
+      content: args.content,
+    });
+    let sessionCode = "";
+    async function generateSessionCode() {
+      sessionCode = Math.random().toString(36).substring(2, 7).toUpperCase();
+      const existingCode = await ctx.db
+        .query("sessionCodes")
+        .withIndex("by_code", (q) => q.eq("code", sessionCode))
+        .collect();
+      if (existingCode.length > 0) {
+        await generateSessionCode();
+      }
+    }
+    await generateSessionCode();
+    await ctx.db.insert("sessionCodes", {
+      code: sessionCode,
+      session: sessionId,
+    });
     return { sessionId, sessionCode };
   },
 });
