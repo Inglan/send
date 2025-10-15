4cc49b18f5030d246582d55d265a61c6f06b20c4
Ingo Wolf <me@ingowolf.au>
2025-10-15 17:18:44 +1100

    attempt to add animations

--- components/account-dropdown.tsx
+++ components/account-dropdown.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { CircleUser, LogIn } from "lucide-react";
 import {
@@ -13,27 +15,52 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Authenticated, AuthLoading, Unauthenticated } from "convex/react";
+import {
+  Authenticated,
+  AuthLoading,
+  Unauthenticated,
+  useConvexAuth,
+} from "convex/react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { motion, AnimatePresence } from "motion/react";
 
 export function AccountDropdown() {
+  const convexAuth = useConvexAuth();
   return (
     <DropdownMenu>
       <Tooltip>
         <DropdownMenuTrigger asChild>
           <TooltipTrigger asChild>
             <Button variant="ghost" size="icon">
-              <Authenticated>
-                <span className="sr-only">Account</span>
-                <CircleUser />
-              </Authenticated>
-              <Unauthenticated>
-                <span className="sr-only">Sign in</span>
-                <LogIn />
-              </Unauthenticated>
-              <AuthLoading>
-                <Skeleton className="size-5 rounded-md" />
-              </AuthLoading>
+              <AnimatePresence>
+                {convexAuth.isLoading ? (
+                  <motion.div
+                    exit={{ opacity: 0 }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                  >
+                    <Skeleton className="size-5 rounded-md" />
+                  </motion.div>
+                ) : convexAuth.isAuthenticated ? (
+                  <motion.div
+                    exit={{ opacity: 0 }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                  >
+                    <span className="sr-only">Account</span>
+                    <CircleUser />
+                  </motion.div>
+                ) : (
+                  <motion.div
+                    exit={{ opacity: 0 }}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                  >
+                    <span className="sr-only">Sign in</span>
+                    <LogIn />
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </Button>
           </TooltipTrigger>
         </DropdownMenuTrigger>
