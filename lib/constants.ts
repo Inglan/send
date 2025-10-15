9943cd7fcc51ddfc9a7890cc76a5593dc811115e
Ingo Wolf <me@ingowolf.au>
2025-10-16 10:08:00 +1100

    update to use types from motion

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,4 @@
+import { Easing } from "motion/react";
+
 export const CODE_LENGTH = 5;
-export const EASE_CURVE = [0.165, 0.84, 0.44, 1.0];
+export const EASE_CURVE: Easing | Easing[] = [0.165, 0.84, 0.44, 1.0];
