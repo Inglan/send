6d5a2a2cf62ca5e79eb6b19d29bdfa25f290b836
Ingo Wolf <me@ingowolf.au>
2025-10-16 10:02:12 +1100

    add auth with google

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,9 +2,10 @@ import { convexAuth, getAuthUserId } from "@convex-dev/auth/server";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
 import { query } from "./_generated/server";
 import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Anonymous, GitHub],
+  providers: [Anonymous, GitHub, Google],
 });
 
 export const getCurrentUser = query({
