f2724b777629c2a4791ffea7a82167206d7540ab
Ingo Wolf <me@ingowolf.au>
2025-10-15 13:42:24 +1100

    change styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const sessionId = useAppState((state) => state.sessionId);
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
+    <div className="flex items-center justify-center min-h-screen">
       {sessionId ? <ContentDisplay /> : <CodeInput />}
     </div>
   );
