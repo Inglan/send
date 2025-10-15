65adf8e34edd4bad49d72bb08184ed6a0e1cf108
Ingo Wolf <me@ingowolf.au>
2025-10-16 07:44:52 +1100

    add getcurrentuser function

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,24 @@
-import { convexAuth } from "@convex-dev/auth/server";
+import { convexAuth, getAuthUserId } from "@convex-dev/auth/server";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
+import { query } from "./_generated/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Anonymous],
+});
+
+export const getCurrentUser = query({
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error("User not authenticated");
+    }
+
+    const user = await ctx.db.get(userId);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return user;
+  },
 });
